65850adc571ed518fc9acae8e28711b5daded0c4
Rida Iqbal <ridariqbal@gmail.com>
2020-05-07 11:24:28 -0400

    feature(data) api route for gettign file paths in progress

---
+++ server/api/parse.js
@@ -0,0 +1,20 @@
+const router = require('express').Router()
+const parse = require('./../services/csvparse')
+
+module.exports = router
+
+router.post('/:userId', (req, res, next) => {
+  try {
+    const getFile = req.body.filepaths
+    getFile.forEach(file => {
+      console.log(file, 'file')
+      let fileName = file.split('/') // to get the file name for the table name
+      fileName = fileName[fileName.length - 1].split('.') //gettting last leg of the path of the file name
+      console.log(fileName[0], 'fileName')
+      parse(`${req.params.userId}-${fileName[0]}`, file) //use last leg of the file name to convert it into a table name in addition to the user.id identifier
+    })
+    res.send({message: 'Sent'})
+  } catch (error) {
+    console.log(error)
+  }
+})
