ca1940b6c8ce6095d03a926d7f53cdff94c0a7cd
Matt Bicknese <mbicknes@villanova.edu>
2020-05-18 21:52:34 -0400

    F#13 - added data selection table within scrollable card containing tabs for switching between tables and tooltips describing each datatype

--- client/components/data-type-row.js
+++ client/components/data-type-row.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {addHeaderType} from '../store/editData'
-import {Button, Grid} from '@material-ui/core'
+import {Button, Grid, Box} from '@material-ui/core'
 import {ToggleButton, ToggleButtonGroup} from '@material-ui/lab'
 import Crop75OutlinedIcon from '@material-ui/icons/Crop75Outlined'
 
@@ -9,7 +9,19 @@ export class DataTypeRow extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      clicked: ''
+      clicked: 'text'
+    }
+  }
+
+  componentDidMount() {
+    if (this.props.clickCheck) {
+      this.setState({clicked: this.props.clickCheck})
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.state.clicked === 'text') {
+      this.props.addHeaderType(this.props.element, 'text', this.props.tableName)
     }
   }
 
@@ -22,58 +34,105 @@ export class DataTypeRow extends Component {
 
   render() {
     return (
-      <Grid>
-        <Button
-          variant="contained"
-          color={this.state.clicked === 'text' ? 'primary' : 'secondary'}
-          onClick={() => this.handleClick('text')}
-        />
-        <Button
-          variant="contained"
-          color={this.state.clicked === 'integer' ? 'primary' : 'secondary'}
-          onClick={() => this.handleClick('integer')}
-        />
-        <Button
-          variant="contained"
-          color={
-            this.state.clicked === 'double precision' ? 'primary' : 'secondary'
-          }
-          onClick={() => this.handleClick('double precision')}
-        />
-        <Button
-          variant="contained"
-          color={this.state.clicked === 'boolean' ? 'primary' : 'secondary'}
-          onClick={() => this.handleClick('boolean')}
-        />
-        <Button
-          variant="contained"
-          color={this.state.clicked === 'date' ? 'primary' : 'secondary'}
-          onClick={() => this.handleClick('date')}
-        />
+      <Grid container direction="row" justify="center">
+        <Box
+          width="10%"
+          height="7%"
+          p={2}
+          m={2}
+          mb={0}
+          sm={1}
+          justifyContent="center"
+          borderRadius={4}
+          border={0}
+          bgcolor="info.main"
+          color="info.contrastText"
+        >
+          {`${this.props.element}`}
+        </Box>
+        <Box p={2} mb={0} sm={1}>
+          <Button
+            style={{
+              maxWidth: '80px',
+              maxHeight: '45px',
+              minWidth: '80px',
+              minHeight: '45px'
+            }}
+            variant="contained"
+            size="medium"
+            color={this.state.clicked === 'text' ? 'primary' : 'default'}
+            onClick={() => this.handleClick('text')}
+          />
+        </Box>
+        <Box p={2} mb={0} sm={1}>
+          <Button
+            style={{
+              maxWidth: '80px',
+              maxHeight: '45px',
+              minWidth: '80px',
+              minHeight: '45px'
+            }}
+            size="medium"
+            variant="contained"
+            color={this.state.clicked === 'integer' ? 'primary' : 'default'}
+            onClick={() => this.handleClick('integer')}
+          />
+        </Box>
+        <Box p={2} mb={0} sm={1}>
+          <Button
+            style={{
+              maxWidth: '80px',
+              maxHeight: '45px',
+              minWidth: '80px',
+              minHeight: '45px'
+            }}
+            size="medium"
+            variant="contained"
+            color={
+              this.state.clicked === 'double precision' ? 'primary' : 'default'
+            }
+            onClick={() => this.handleClick('double precision')}
+          />
+        </Box>
+        <Box p={2} mb={0} sm={1}>
+          <Button
+            style={{
+              maxWidth: '80px',
+              maxHeight: '45px',
+              minWidth: '80px',
+              minHeight: '45px'
+            }}
+            size="medium"
+            variant="contained"
+            color={this.state.clicked === 'boolean' ? 'primary' : 'default'}
+            onClick={() => this.handleClick('boolean')}
+          />
+        </Box>
+        <Box p={2} mb={0} sm={1}>
+          <Button
+            style={{
+              maxWidth: '80px',
+              maxHeight: '45px',
+              minWidth: '80px',
+              minHeight: '45px'
+            }}
+            size="medium"
+            variant="contained"
+            color={this.state.clicked === 'date' ? 'primary' : 'default'}
+            onClick={() => this.handleClick('date')}
+          />
+        </Box>
       </Grid>
-      // <tr>
-      //   <td>{props.element}</td>
-      //   <td>
-      //     <select
-      //       onChange={e =>
-      //         props.addHeaderType(props.element, e.target.value, props.tableName)
-      //       }
-      //       className="dropdown"
-      //     >
-      //       <option value="">Choose Type</option>
-      //       <option value="integer">Integer</option>
-      //       <option value="double precision">Float</option>
-      //       <option value="text">Text</option>
-      //       <option value="boolean">Boolean</option>
-      //       <option value="date">Date</option>
-      //     </select>
-      //   </td>
-      // </tr>
     )
   }
 }
 
-const mapStateToProps = state => ({})
+const mapStateToProps = (state, ownProps) => ({
+  clickCheck:
+    state.tableData[ownProps.index][ownProps.tableName].headers[
+      ownProps.element
+    ]
+})
 
 const mapDispatchToProps = dispatch => {
   return {
