2f941b6e7647bb48c8670f90145013b8fe232815
Rida Iqbal <ridariqbal@gmail.com>
2020-05-23 14:58:44 -0400

    Fix - cleaned up Join Steps code

--- client/components/Joins/join-steps.js
+++ client/components/Joins/join-steps.js
@@ -1,20 +1,12 @@
 import React from 'react'
-import {Grid, Typography} from '@material-ui/core'
-// import {gray} from '@material-ui/core/colors'
+import {Typography} from '@material-ui/core'
 import {SimpleCard} from '../home'
 import HelpIcon from '@material-ui/icons/HelpOutlineOutlined'
-// import {style, useStyles} from './join-styles'
 import clsx from 'clsx'
 import {makeStyles} from '@material-ui/core/styles'
 import Drawer from '@material-ui/core/Drawer'
 import Button from '@material-ui/core/Button'
-import List from '@material-ui/core/List'
 import Divider from '@material-ui/core/Divider'
-import ListItem from '@material-ui/core/ListItem'
-import ListItemIcon from '@material-ui/core/ListItemIcon'
-import ListItemText from '@material-ui/core/ListItemText'
-import InboxIcon from '@material-ui/icons/MoveToInbox'
-import MailIcon from '@material-ui/icons/Mail'
 
 const useStyles = makeStyles({
   list: {
